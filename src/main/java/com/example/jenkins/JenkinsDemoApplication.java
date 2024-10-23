7993227607b31b7a9d2c6e6781ea2498564890e8
Bharath <ubharath.290403@gmail.com>
2024-10-23 19:23:59 -0400

    first commit

---
+++ src/main/java/com/example/jenkins/JenkinsDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkins;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	public static void main(String[] args) {
+
+		logger.info("This is the logger statement");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/JenkinsDemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.jenkins;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("In Testing......");
+		Assertions.assertEquals(true,true);
+	}
+
+}
