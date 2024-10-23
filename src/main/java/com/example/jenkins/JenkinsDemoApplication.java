2315114d689689ae21d80428d55963fc097a4608
Bharath <ubharath.290403@gmail.com>
2024-10-23 19:42:52 -0400

    second commit

--- src/main/java/com/example/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkins/JenkinsDemoApplication.java
@@ -11,6 +11,7 @@ public class JenkinsDemoApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 
+
 	public static void main(String[] args) {
 
 		logger.info("This is the logger statement");
